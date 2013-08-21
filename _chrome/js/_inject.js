19642cae79db087319da6de724cc3445a76669db
boblauer <rlauer@gmail.com>
2013-08-20 23:31:59 -0500

    Removing generated content

--- _chrome/js/_inject.js
+++
@@ -1,8 +0,0 @@
-var s = document.createElement('script');
-s.src = chrome.extension.getURL("js/ghplus.js");
-console.log(chrome.extension.getURL(''));
-s.onload = function() {
-    this.parentNode.removeChild(this);
-};
-console.log(s.src);
-(document.head||document.documentElement).appendChild(s);
